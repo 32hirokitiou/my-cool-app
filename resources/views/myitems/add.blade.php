c7ba04765608c8e4a36787d135c789e7dc9c0ff9
Hirokiitou <itt.pedro5328@gmail.com>
2020-08-02 19:33:14 +0900

    retouching myitems view->_add blade

--- resources/views/myitems/add.blade.php
+++ resources/views/myitems/add.blade.php
@@ -1,19 +1,13 @@
 テストひょうじ
-((-- aa --))
+投稿作成<br>
 
+<form action='{{ action('MyitemsController@create') }}' method='post'>
 
-@section('article_add')
-    投稿作成<br>
-
-    <form action='{{ route('myitems_create') }}' method='post'>
+        ユーザーID：<input type='text' name='user_id'><br>
+        タイトル：<input type='text' name='title'><br>
+        内容：<input type='text' name='content'><br>
+        写真：
+        <input type='submit' value='投稿する'>
         {{ csrf_field() }}
-        A
-            ユーザーID：<input type='text' name='user_id'><br>
-            タイトル：<input type='text' name='title'><br>
-            内容：<input type='text' name='content'><br>
-            <input type='submit' value='投稿'>
-    </form>
-@endsection --> --))
+</form>
 
-
-Auth::id();
