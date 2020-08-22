2b95dcf227b02a84a54806eff795d342490e487d
Hirokiitou <itt.pedro5328@gmail.com>
2020-08-22 17:01:43 +0900

    Fix show&showdetail blade

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,6 +1,37 @@
-@extends('layouts.posts')
-@section('title', '登録済みニュースの一覧')
-@section('content')
+<!DOCTYPE html>
+<link rel="stylesheet" href="{{ asset('/css/posts.css') }}">
+<link rel="stylesheet" href="{{ asset('/css/common.css') }}">
+<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+@extends('layouts.common')
+@section('contents')
+
+
+<a href="{{ action('PostsController@add') }}" role="button" class="btn btn-primary">新規作成</a>
+
+<div id="cardlayout-wrap">
+    @foreach($posts as $post)
+    <section class="card-list">
+        <a class="card-link">
+            <figure class="card-figure"><a href="/posts/{{ $post->id }}"><img src="{{ asset('storage/image/'.$post->image_path)}}"></figure>
+            <h2 class="card-title">{{ \Str::limit($post->title, 100) }}</h2>
+        </a>
+        <div>
+            <a href="{{ action('PostsController@edit', ['id' => $post->id]) }}">編集</a>
+        </div>
+        <div>
+            <a href="{{ action('PostsController@delete', ['id' => $post->id]) }}">削除</a>
+        </div>
+        <div class="form-text text-info">
+            現在選択中のタグ: @foreach ($post->tags as $tag) {{ $tag->name }} {{"\n"}} @endforeach
+        </div>
+    </section>
+    @endforeach
+</div>
+@endsection
+
+
+<a href="{{ action('PostsController@add') }}" role="button" class="btn btn-primary">新規作成</a>
 <div class="container">
     <div class="row">
         <h2>マイページ投稿一覧</h2>
@@ -62,4 +93,3 @@
     </div>
 </div>
 </div>
-@endsection
